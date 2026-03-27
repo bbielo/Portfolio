4a84a7c3155401942610073af8b9a251494eefa7
bbielo <eulbi90@gmail.com>
2026-03-27 16:33:43 +0900

    fix: ProjectsSection-iframe 제거

--- src/sections/ProjectsSection.vue
+++ src/sections/ProjectsSection.vue
@@ -5,12 +5,13 @@
         <h2 class="text-2xl font-semibold text-zinc-900">Toy Projects</h2>
 
         <div class="mt-8 rounded-3xl bg-white p-10 shadow-sm overflow-visible">
-          <iframe
+          <!-- <iframe
             src="https://bbielo.github.io/embed/work.html?v=20260212-3"
             class="w-full h-[730px] block"
             scrolling="no"
             style="border:0;"
-          />
+          /> -->
+          <span>업데이트 예정</span>
         </div>
       </div>
     </div>
